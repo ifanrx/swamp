6b3dbea50b5e3f62e48321158106b15b623771f4
quanhua.peng <pengquanhua@ifanr.com>
2020-11-04 13:32:39 +0800

    swift5

--- Example/Pods/Nimble/Sources/Nimble/Nimble.h
+++ Example/Pods/Nimble/Sources/Nimble/Nimble.h
@@ -3,5 +3,10 @@
 #import "NMBStringify.h"
 #import "DSL.h"
 
+#if TARGET_OS_OSX || TARGET_OS_IOS
+    #import "CwlMachBadInstructionHandler.h"
+    #import "CwlCatchException.h"
+#endif
+
 FOUNDATION_EXPORT double NimbleVersionNumber;
 FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
